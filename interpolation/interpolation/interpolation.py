9825571a1a7e62926caa812a025d0429fc289206
Andrey Kirkicha <kirkicha.andrey@mail.ru>
2025-03-05 22:24:23 +0300

    Bilinear interpolation for images
    New:
      -- Interpolation algorithm adapted for image processing
    
    TODO:
      -- Implement CLI-interface
      -- Add other interpolation methods
      -- Optimize existing algorithm

--- interpolation/interpolation/interpolation.py
+++ interpolation/interpolation/interpolation.py
@@ -1,15 +1,37 @@
-# F -- 2D array of function values
-# x1, x2, y1, y2 -- points of known function values
-# x, y -- point to interpolate in
-def bilinear_interpolation(F, x, y, x1, x2, y1, y2):
-    Q11 = F(x1, y1)
-    Q12 = F(x1, y2)
-    Q21 = F(x2, y1)
-    Q22 = F(x2, y2)
+from PIL import Image
+import numpy as np
 
-    F_int = 1 / (x2 - x1) / (y2 - y1) * ((x2 - x) * (y2 - y) * Q11 +
-                                         (x - x1) * (y2 - y) * Q21 + 
-                                         (x2 - x) * (y - y1) * Q12 + 
-                                         (x - x1) * (y - y1) * Q22)
+def bilinear_interpolation(img, new_width, new_height):
+    width, height = img.size
 
-    return F_int
+    resized_img = Image.new("RGB", (new_width, new_height))
+
+    x_ratio = width / float(new_width)
+    y_ratio = height / float(new_height)
+
+    for i in range(new_height):
+        for j in range(new_width):
+            x = j * x_ratio
+            y = i * y_ratio
+
+            pix = [0, 0, 0]
+
+            x1 = int(x)
+            y1 = int(y)
+            x2 = min(x1 + 1, width - 1)
+            y2 = min(y1 + 1, height - 1)
+
+            Q11 = img.getpixel((x1, y1))
+            Q12 = img.getpixel((x1, y2))
+            Q21 = img.getpixel((x2, y1))
+            Q22 = img.getpixel((x2, y2))
+
+            for ch in range(3):
+                pix[ch] = int(1 / (x2 - x1) / (y2 - y1) * (Q11[ch] * (x2 - x) * (y2 - y)
+                                                        + Q21[ch] * (x - x1) * (y2 - y)
+                                                        + Q12[ch] * (x2 - x) * (y - y1)
+                                                        + Q22[ch] * (x - x1) * (y - y1))) if (x2 - x1) != 0 and (y2 - y1) != 0 else 0
+
+            resized_img.putpixel((j, i), tuple(pix))
+    
+    return resized_img
--- interpolation/tests/tests.py
+++ interpolation/tests/tests.py
@@ -1,66 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import click
+from PIL import Image
 from interpolation.interpolation import bilinear_interpolation
 from mpl_toolkits.mplot3d import Axes3D
 
-def F(x, y):
-    return np.sin(x + y)
-
-def find_closest_values(array, target):
-    id = (np.abs(array - target)).argmin()
-
-    if array[id] < target:
-        val1 = array[id]
-        val2 = array[id + 1]
-    else:
-        val1 = array[id - 1]
-        val2 = array[id]
-    
-    return val1, val2
-
+@click.command()
 def main():
-    x = np.linspace(-np.pi, np.pi, 10)
-    y = np.linspace(-np.pi, np.pi, 10)
-    X, Y = np.meshgrid(x, y)
-    Z = F(X, Y)
+    image = Image.open("tests/test_image.jpg")
+    width, height = image.size
 
-    arr_x_int = np.linspace(-np.pi + 0.01, np.pi - 0.01, 100)
-    arr_y_int = np.linspace(-np.pi + 0.01, np.pi - 0.01, 100)
-    X_int, Y_int = np.meshgrid(arr_x_int, arr_y_int)
-    Z_int = np.zeros_like(X_int)
-
-    for i in range(X_int.shape[0]):
-        for j in range(X_int.shape[1]):
-            x_int = X_int[i, j]
-            y_int = Y_int[i, j]
-
-            x1, x2 = find_closest_values(x, x_int)
-            y1, y2 = find_closest_values(y, y_int)
-
-            Z_int[i, j] = bilinear_interpolation(F, x_int, y_int, x1, x2, y1, y2)
-
-    fig = plt.figure(figsize=(16, 10))
-    
-    ax1 = fig.add_subplot(121, projection='3d')
-    surf1 = ax1.plot_wireframe(X, Y, Z, alpha=0.5, label='Original function')
-    points1 = ax1.scatter(X, Y, Z, alpha=0.5)
-    ax1.set_xlabel('X')
-    ax1.set_ylabel('Y')
-    ax1.set_zlabel('Z')
-    ax1.legend()
-
-    ax2 = fig.add_subplot(122, projection='3d')
-    surf2 = ax2.scatter(X_int, Y_int, Z_int, alpha=0.2, label='Bilinear interplotaion')
-    ax2.set_xlabel('X')
-    ax2.set_ylabel('Y')
-    ax2.set_zlabel('Z')
-    ax2.legend()
-
-    filename = "tests/plots_bilinear.png"
-    plt.savefig(filename)
-    plt.close(fig)
-
-    plt.show()
+    res_image = bilinear_interpolation(image, width * 2, height * 2)
+    res_image.save("tests/result.jpg")
 
 if __name__ == "__main__":
     main()
