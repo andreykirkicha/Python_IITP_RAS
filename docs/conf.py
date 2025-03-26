8e1eae444ac8678d985bc7986b4408f41988afd0
Andrey Kirkicha <kirkicha.andrey@mail.ru>
2025-03-26 23:37:10 +0300

    Initial documentation
    NEW:
      -- Documentation added using 'sphinx-quickstart'
    
    TODO:
      -- Manage extensions
      -- Add more pages

---
+++ docs/conf.py
@@ -0,0 +1,60 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Python_IITP_RAS'
+copyright = '2025, Andrey Kirkicha'
+author = 'Andrey Kirkicha'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',  # Core library for auto html generation from docstrings
+    'sphinx_rtd_theme',
+    'sphinx_click', # For documenting Click command-line interfaces
+    'myst_parser'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+source_suffix = {
+    '.rst': 'markdown',
+    '.md': 'markdown'
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Options for autodoc ----------------------------------------------------
+
+autodoc_member_order = 'bysource'
+autodoc_typehints = "description"
+
+# -- Options for MyST ---------------------------------------------------
+
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_block",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
