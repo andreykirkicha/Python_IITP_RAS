6b538d3b5c1415c13518e352ebf70f0d66619650
Andrey Kirkicha <kirkicha.andrey@mail.ru>
2025-04-14 18:43:42 +0300

    Added docs generation to noxfile.py
    NEW:
      -- Docs generation using nox
    
    TODO:
      -- Clean docs/ folder

--- noxfile.py
+++ noxfile.py
@@ -3,8 +3,8 @@ import nox_poetry
 from nox import Session
 
 package = "interpolation"
-nox.options.sessions = "linter", "formatter", "pytest"
-# nox.options.sessions = ["pytest"]
+nox.options.sessions = "linter", "formatter", "pytest", "documentation"
+# nox.options.sessions = ["documentation"]
 locations = "interpolation", "example", "docs", "tests"
 
 
@@ -30,3 +30,16 @@ def pytest(session: Session) -> None:
     args = session.posargs or ["--cov=interpolation"]
     session.install("pytest", "pytest-cov", "Pillow", "numpy")
     session.run("pytest", *args)
+
+
+@nox_poetry.session(python="3.12")
+def documentation(session: Session) -> None:
+    """Generate documentation."""
+    session.install("sphinx",
+                    "sphinx_rtd_theme",
+                    "sphinx_click",
+                    "myst_parser",
+                    "linkify-it-py")
+    
+    session.run("python", "-c", "import sys; sys.path.insert(0, '.')")
+    session.run("sphinx-build", "-M", "html", "docs/source/", "docs/build/")
